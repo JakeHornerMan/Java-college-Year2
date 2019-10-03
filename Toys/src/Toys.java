5591bf85319c4be33b07767e5a6372317c4a5abb
Jake Horner <jakehorner19@gmail.com>
2019-10-03 14:21:59 +0100

    Add files via upload

---
+++ Toys/src/Toys.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toys;
+
+/**
+ *
+ * @author x16318711
+ */
+public class Toys {
+    
+    public String name;
+    public String price;
+    public String dis;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDis() {
+        return dis;
+    }
+
+    public void setDis(String dis) {
+        this.dis = dis;
+    }
+    
+}
---
+++ Toys/src/ToysApp.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author x16318711
+ */
+public class ToysApp {
+     public static void main(String[] args) {
+        // TODO code application logic here
+        ToysGUI myGUI = new ToysGUI();
+        myGUI.setVisible(true);
+     }
+}
